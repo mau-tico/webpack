3165a5238da4d27db7e9bf7532110c268a96549a
mau <mauriciorj76@gmail.com>
2021-05-11 18:59:21 -0600

    un vergo de cosas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import Template from './templates/Template.js';
+/* import Template from './templates/Template.js';
 import './styles/main.css';
+import './styles/vars.styl'; */
+
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
 
 (async function App() {
   const main = null || document.getElementById('main');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
 
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
     },
 
     module: {
@@ -19,7 +31,33 @@ module.exports = {
             use: {
                 loader: 'babel-loader'
             }
-        }]
+        },
+        {
+            test: /\.css|.styl$/i,
+            use: [MiniCssExtractPlugin.loader,
+            'css-loader',
+            'stylus-loader'
+        ],
+        },
+        {
+            test: /\.png/,
+            type: 'asset/resource'
+        },
+        {
+            test: /\.(woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
+                    esModule: false,
+                },
+            }
+        }
+    ]
     },
 
     plugins: [
@@ -27,6 +65,25 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html'
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new Dotenv(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
