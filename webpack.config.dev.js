a3971fce06f31faadec6ea380dd4cebc869c76f1
mau <mauriciorj76@gmail.com>
2021-05-11 19:26:25 -0600

    caca

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,6 +12,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
+    //watch: true, //Se queda observando los cambios sin compilar //prueba
     resolve: {
         extensions: ['.js'],
         alias: {
